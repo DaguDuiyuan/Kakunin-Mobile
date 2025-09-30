049143d25d08eb8ef92f72819456c0b4470547db
dagu <1184313565@qq.com>
2025-09-30 14:02:43 +0800

    fix

--- android/app/src/main/kotlin/com/example/kakunin/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/kakunin/MainActivity.kt
@@ -2,23 +2,5 @@ package cc.zsakvo.otp
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-//class MainActivity: FlutterFragmentActivity() {
-//}
 
-
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
-import android.util.Log
-import androidx.annotation.NonNull
-import androidx.documentfile.provider.DocumentFile
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import java.io.FileDescriptor
-import java.io.FileOutputStream
-
-class MainActivity : FlutterActivity() {
-}
-
+class MainActivity: FlutterFragmentActivity()
